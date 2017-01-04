ae110520371d526026a166e816013dd37598cc8f
夏琳泰 <xltxlm@qq.com>
2017-01-04 17:16:17 +0800

    初始化项目

---
+++ src/TestConfig.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialintai
+ * Date: 2017/1/4
+ * Time: 17:15
+ */
+
+namespace xltxlm\config;
+
+
+interface TestConfig
+{
+    /**
+     * 实现测试的接口
+     * @return mixed
+     */
+    public function test();
+}
